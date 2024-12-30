ff32d353e9b100818b58e0cbb3cea270771afe26
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2024-12-30 10:19:16 +0100

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,23 +6,23 @@ import twitter from "../assets/logo/twiitter x.jpeg";
 
 const Footer = () => {
   return (
-    <footer>
-      <div className="social-links">
+    <footer className="bg-purple-700 mt-12 p-3 flex flex-col justify-center items-center text-white">
+      <div className="social-links flex">
         <a className="sm-link" href="#">
-          <img src={facebook} alt="facebook" />
+          <img className="w-8" src={facebook} alt="facebook" />
         </a>
         <a className="sm-link" href="#">
-          <img src={insta} alt="insta" />
+          <img className="w-8" src={insta} alt="insta" />
         </a>
         <a className="sm-link" href="#">
-          <img src={linkedin} alt="linkedin" />
+          <img className="w-8" src={linkedin} alt="linkedin" />
         </a>
         <a className="sm-link" href="#">
-          <img src={twitter} alt="twitter" />
+          <img className="w-8" src={twitter} alt="twitter" />
         </a>
       </div>
-      <div className="copy">
-        <p>&copy; Copyright Bayzel Integrated Services @2025</p>
+      <div className="copy flex flex-col items-center mt-20">
+        <p>&copy; Copyright Everistus Nwogo @2025</p>
         <p>All Rights Reserved</p>
       </div>
     </footer>
