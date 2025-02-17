d6a1eb5758e11abcfd3afc7faf2e1c4e2effc57c
guavacoderepo <guavacodeplus@gmail.com>
2025-02-17 14:39:53 +0000

    push from evan

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,5 +1,5 @@
-import projectimg from "./images/sample_exp_pic.png";
-import edaProject from "./images/eda.png";
-import edaProject2 from "./images/eda_2.png";
+import projectimg from "./sample_exp_pic.png";
+import edaProject from "./eda.png";
+import edaProject2 from "./eda_2.png";
 
 export { projectimg, edaProject, edaProject2 };
