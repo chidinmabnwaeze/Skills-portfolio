eb9958ae0dd5a289eb57a802720808dda0d39608
guavacoderepo <guavacodeplus@gmail.com>
2025-02-17 15:49:03 +0000

    push from evan

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -134,7 +134,7 @@ const Home = () => {
             /> */}
 
             <div className="info ml-10">
-              <article className="text-xlg">
+              <article className="text-2xl text-center">
                 A detail-oriented and experienced engineer with over 2 years of
                 experience in software design, development, and analysis,
                 complemented by my AI engineering skills. I have a proven track
