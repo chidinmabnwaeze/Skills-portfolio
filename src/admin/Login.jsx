8b77cfc3bdd9524694fbf2104cd5cfaa3eb269c6
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2025-03-28 14:06:37 +0100

    styled dropdown

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -5,10 +5,10 @@ const Login = () => {
     <div className=" h-screen flex justify-center items-center bg-purple-100">
       <section>
         <h1 className="font-bold text-3xl">Welcome to your Portfolio !</h1>
-        <div className="border p-5">
-          <h1>Who is signing in ?</h1>
+        <div className="border p-5 flex flex-col justify-center items-center">
+          <h1 className="font-medium text-lg m-3">Who is signing in ?</h1>
           <div className="dropdown">
-            <select name="name" id="">
+            <select name="name" id="" className="w-[300px] p-3">
               <option value=""></option>
               <option value="name">Chidinma</option>
               <option value="name">Everistus</option>
