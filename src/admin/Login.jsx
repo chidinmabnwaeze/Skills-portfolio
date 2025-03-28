f43b1e21d2ca83f99355e21710ac969ec940dcf5
chidinmabnwaeze <138997346+chidinmabnwaeze@users.noreply.github.com>
2025-03-28 13:57:05 +0100

    dropdown

--- src/admin/Login.jsx
+++ src/admin/Login.jsx
@@ -2,14 +2,22 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Welcome to your Portfolio !</h1>
-      <div className="border p-5">
-        <h1>Who is signing in ?</h1>
-        <div className="dropdown">
-          <p>Chidinma</p>
+    <div className=" h-screen flex justify-center items-center bg-purple-100">
+      <section>
+        <h1 className="font-bold text-3xl">Welcome to your Portfolio !</h1>
+        <div className="border p-5">
+          <h1>Who is signing in ?</h1>
+          <div className="dropdown">
+            <select name="name" id="">
+              <option value=""></option>
+              <option value="name">Chidinma</option>
+              <option value="name">Everistus</option>
+            </select>
+
+            <p>Chidinma</p>
+          </div>
         </div>
-      </div>
+      </section>
     </div>
   );
 };
